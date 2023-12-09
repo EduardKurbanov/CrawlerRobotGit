8b6cbe5813c8fe5badcac4e8bdbe405d43ea9da0
Edward <edward89@ukr.net>
2023-12-09 10:49:31 +0200

    add axix_rotation_angle_x, axix_rotation_angle_y (mpu6050)

--- robot.py
+++ robot.py
@@ -4,6 +4,7 @@ import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.GPIO as GPIO
 import smbus
 import time
+import math
 
 class Robot:
     """
@@ -115,88 +116,99 @@ class Robot:
         # address MPU-6050 in i2c
         device_address = 0x68
         # MPU-6050 Registers
-        _MPU6050_PWR_MGMT_1 = 0x6B  # Primary power/sleep control register
-        _MPU6050_PWR_MGMT_2 = 0x6C
-        _GYRO_CONFIG = 0x1B
-        _ACCEL_CONFIG = 0x1C
-        _ACCEL_X_OUT_H = 0x3B
-        _ACCEL_X_OUT_L = 0x3C
-        _ACCEL_Y_OUT_H = 0x3D
-        _ACCEL_Y_OUT_L = 0x3E
-        _ACCEL_Z_OUT_H = 0x3F
-        _ACCEL_Z_OUT_L = 0x40
-        _TEMP_OUT_H = 0X41
-        _TEMP_OUT_L = 0X42
-        _GYRO_X_OUT_H = 0x43
-        _GYRO_X_OUT_L = 0x44
-        _GYRO_Y_OUT_H = 0x45
-        _GYRO_Y_OUT_L = 0x46
-        _GYRO_Z_OUT_H = 0x47
-        _GYRO_Z_OUT_L = 0x48
+        _REGISTER_MPU6050_PWR_MGMT_1 = 0x6B  # Primary power/sleep control register
+        _REGISTER_MPU6050_PWR_MGMT_2 = 0x6C
+        _REGISTER_GYRO_CONFIG = 0x1B
+        _REGISTER_ACCEL_CONFIG = 0x1C
+        _REGISTER_ACCEL_X_OUT_H = 0x3B
+        _REGISTER_ACCEL_X_OUT_L = 0x3C
+        _REGISTER_ACCEL_Y_OUT_H = 0x3D
+        _REGISTER_ACCEL_Y_OUT_L = 0x3E
+        _REGISTER_ACCEL_Z_OUT_H = 0x3F
+        _REGISTER_ACCEL_Z_OUT_L = 0x40
+        _REGISTER_TEMP_OUT_H = 0X41
+        _REGISTER_TEMP_OUT_L = 0X42
+        _REGISTER_GYRO_X_OUT_H = 0x43
+        _REGISTER_GYRO_X_OUT_L = 0x44
+        _REGISTER_GYRO_Y_OUT_H = 0x45
+        _REGISTER_GYRO_Y_OUT_L = 0x46
+        _REGISTER_GYRO_Z_OUT_H = 0x47
+        _REGISTER_GYRO_Z_OUT_L = 0x48
         # Pre-defined ranges
-        _ACCEL_RANGE_2G = 0x00
-        _ACCEL_RANGE_4G = 0x08
-        _ACCEL_RANGE_8G = 0x10
-        _ACCEL_RANGE_16G = 0x18
-        _GYRO_RANGE_250DEG = 0x00
-        _GYRO_RANGE_500DEG = 0x08
-        _GYRO_RANGE_1000DEG = 0x10
-        _GYRO_RANGE_2000DEG = 0x18
+        _REGISTER_ACCEL_RANGE_2G = 0x00
+        _REGISTER_ACCEL_RANGE_4G = 0x08
+        _REGISTER_ACCEL_RANGE_8G = 0x10
+        _REGISTER_ACCEL_RANGE_16G = 0x18
+        _REGISTER_GYRO_RANGE_250DEG = 0x00
+        _REGISTER_GYRO_RANGE_500DEG = 0x08
+        _REGISTER_GYRO_RANGE_1000DEG = 0x10
+        _REGISTER_GYRO_RANGE_2000DEG = 0x18
         # earth gravity
         _STANDARD_GRAVITY = 9.80665
         # Modifiers accel
-        _RANGE_2_G_16384_LSB = 16384.0  # +/- 2g (default value), 16384 LSB/G (default value)
-        _RANGE_4_G_8192_LSB = 8192.0  # +/- 4g, 8192 LSB/G
-        _RANGE_8_G_4096_LSB = 4096.0  # +/- 8g, 4096 LSB/G
-        _RANGE_16_G_2048_LSB = 2048.0  # +/- 16g, 2048 LSB/G
+        _ACCEL_RANGE_2_G_16384_LSB = 16384.0  # +/- 2g (default value), 16384 LSB/G (default value)
+        _ACCEL_RANGE_4_G_8192_LSB = 8192.0  # +/- 4g, 8192 LSB/G
+        _ACCEL_RANGE_8_G_4096_LSB = 4096.0  # +/- 8g, 4096 LSB/G
+        _ACCEL_RANGE_16_G_2048_LSB = 2048.0  # +/- 16g, 2048 LSB/G
         # Modifiers gyro
-        _RANGE_250_DPS = 131.0  # +/- 250 deg/s (default value)
-        _RANGE_500_DPS = 65.5  # +/- 500 deg/s
-        _RANGE_1000_DPS = 32.8  # +/- 1000 deg/s
-        _RANGE_2000_DPS = 16.4  # +/- 2000 deg/s
+        _GYRO_RANGE_250_DPS = 131.0  # +/- 250 deg/s (default value)
+        _GYRO_RANGE_500_DPS = 65.5  # +/- 500 deg/s
+        _GYRO_RANGE_1000_DPS = 32.8  # +/- 1000 deg/s
+        _GYRO_RANGE_2000_DPS = 16.4  # +/- 2000 deg/s
 
         bus = self.i2c_bus  # I2C bus number used, may sometimes change on your system
-        bus.write_byte_data(device_address, _MPU6050_PWR_MGMT_1, 0)  # enable MPU6050
-        bus.write_byte_data(device_address, _ACCEL_CONFIG, _ACCEL_RANGE_16G)  # set 16g range for accel
-        bus.write_byte_data(device_address, _GYRO_CONFIG, _GYRO_RANGE_2000DEG)  # set 2000 range for gyro
+        bus.write_byte_data(device_address, _REGISTER_MPU6050_PWR_MGMT_1, 0x00)  # enable MPU6050
+        bus.write_byte_data(device_address, _REGISTER_ACCEL_CONFIG, _REGISTER_ACCEL_RANGE_16G)  # set 16g range for accel
+        bus.write_byte_data(device_address, _REGISTER_GYRO_CONFIG, _REGISTER_GYRO_RANGE_2000DEG)  # set 2000 range for gyro
         time.sleep(1)
 
-        accel_x_h = bus.read_byte_data(device_address, _ACCEL_X_OUT_H)
-        accel_x_l = bus.read_byte_data(device_address, _ACCEL_X_OUT_L)
+        accel_x_h = bus.read_byte_data(device_address, _REGISTER_ACCEL_X_OUT_H)
+        accel_x_l = bus.read_byte_data(device_address, _REGISTER_ACCEL_X_OUT_L)
         signed_int_assel_x = c_short((accel_x_h << 8) | accel_x_l).value
-        accel_x = (signed_int_assel_x / _RANGE_16_G_2048_LSB) * _STANDARD_GRAVITY
+        accel_x = (signed_int_assel_x / _ACCEL_RANGE_16_G_2048_LSB)
+        accel_x_gravity = accel_x * _STANDARD_GRAVITY
 
-        accel_y_h = bus.read_byte_data(device_address, _ACCEL_Y_OUT_H)
-        accel_y_l = bus.read_byte_data(device_address, _ACCEL_Y_OUT_L)
+        accel_y_h = bus.read_byte_data(device_address, _REGISTER_ACCEL_Y_OUT_H)
+        accel_y_l = bus.read_byte_data(device_address, _REGISTER_ACCEL_Y_OUT_L)
         signed_int_assel_y = c_short((accel_y_h << 8) | accel_y_l).value
-        accel_y = (signed_int_assel_y / _RANGE_16_G_2048_LSB) * _STANDARD_GRAVITY
+        accel_y = (signed_int_assel_y / _ACCEL_RANGE_16_G_2048_LSB)
+        accel_y_gravity = accel_y * _STANDARD_GRAVITY
 
-        accel_z_h = bus.read_byte_data(device_address, _ACCEL_Z_OUT_H)
-        accel_z_l = bus.read_byte_data(device_address, _ACCEL_Z_OUT_L)
+        accel_z_h = bus.read_byte_data(device_address, _REGISTER_ACCEL_Z_OUT_H)
+        accel_z_l = bus.read_byte_data(device_address, _REGISTER_ACCEL_Z_OUT_L)
         signed_int_assel_z = c_short((accel_z_h << 8) | accel_z_l).value
-        accel_z = (signed_int_assel_z / _RANGE_16_G_2048_LSB) * _STANDARD_GRAVITY
+        accel_z = (signed_int_assel_z / _ACCEL_RANGE_16_G_2048_LSB)
+        accel_z_gravity = accel_z * _STANDARD_GRAVITY
 
-        temp_hex_h = bus.read_byte_data(device_address, _TEMP_OUT_H)
-        temp_hex_l = bus.read_byte_data(device_address, _TEMP_OUT_L)
+        temp_hex_h = bus.read_byte_data(device_address, _REGISTER_TEMP_OUT_H)
+        temp_hex_l = bus.read_byte_data(device_address, _REGISTER_TEMP_OUT_L)
         signed_int_temp = c_short((temp_hex_h << 8) | temp_hex_l).value
         temp = ((signed_int_temp) / 340) + 36.53
 
-        gyro_x_h = bus.read_byte_data(device_address, _GYRO_X_OUT_H)
-        gyro_x_l = bus.read_byte_data(device_address, _GYRO_X_OUT_L)
+        gyro_x_h = bus.read_byte_data(device_address, _REGISTER_GYRO_X_OUT_H)
+        gyro_x_l = bus.read_byte_data(device_address, _REGISTER_GYRO_X_OUT_L)
         signed_int_gyro_x = c_short((gyro_x_h << 8) | gyro_x_l).value
-        gyro_x = ((signed_int_gyro_x) / _RANGE_2000_DPS)
+        gyro_x = ((signed_int_gyro_x) / _GYRO_RANGE_2000_DPS)
 
-        gyro_y_h = bus.read_byte_data(device_address, _GYRO_Y_OUT_H)
-        gyro_y_l = bus.read_byte_data(device_address, _GYRO_Y_OUT_L)
+        gyro_y_h = bus.read_byte_data(device_address, _REGISTER_GYRO_Y_OUT_H)
+        gyro_y_l = bus.read_byte_data(device_address, _REGISTER_GYRO_Y_OUT_L)
         signed_int_gyro_y = c_short((gyro_y_h << 8) | gyro_y_l).value
-        gyro_y = ((signed_int_gyro_y) / _RANGE_2000_DPS)
+        gyro_y = ((signed_int_gyro_y) / _GYRO_RANGE_2000_DPS)
 
-        gyro_z_h = bus.read_byte_data(device_address, _GYRO_Z_OUT_H)
-        gyro_z_l = bus.read_byte_data(device_address, _GYRO_Z_OUT_L)
+        gyro_z_h = bus.read_byte_data(device_address, _REGISTER_GYRO_Z_OUT_H)
+        gyro_z_l = bus.read_byte_data(device_address, _REGISTER_GYRO_Z_OUT_L)
         signed_int_gyro_z = c_short((gyro_z_h << 8) | gyro_z_l).value
-        gyro_z = ((signed_int_gyro_z) / _RANGE_2000_DPS)
+        gyro_z = ((signed_int_gyro_z) / _GYRO_RANGE_2000_DPS)
 
-        return (accel_x, accel_y, accel_z, temp, gyro_x, gyro_y, gyro_z)
+        axix_rotation_angle_x = math.degrees(
+            math.atan2(float(accel_y), math.sqrt(float(accel_x) ** 2 + float(accel_z) ** 2)))
+
+        axix_rotation_angle_y = math.degrees(
+            math.atan2(-float(accel_x), math.sqrt(float(accel_y) ** 2 + float(accel_z) ** 2)))
+
+        return (accel_x_gravity, accel_y_gravity, accel_z_gravity, temp, gyro_x, gyro_y, gyro_z, axix_rotation_angle_x,
+                axix_rotation_angle_y)
+
 
     def driver_motor_robot(self, motor_action: str = "") -> None:
         """
