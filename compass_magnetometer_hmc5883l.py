29f01c3bf502da32ebe9f6484539a5f4da93ef18
Edward <edward89@ukr.net>
2023-12-10 12:04:28 +0200

    add class hmc5883l

---
+++ compass_magnetometer_hmc5883l.py
@@ -0,0 +1,17 @@
+from typing import Tuple, List
+from ctypes import c_short
+import smbus
+import time
+import math
+
+class HMC5883L(object):
+    """
+        for mini computer PocketBeagle®,
+        model compass magnetometer: HMC5883L,
+        protocol: i2c,
+        i2c_pin_sda: 'P1_26'
+        i2c_pin_scl: 'P1_28'
+    """
+    def __init__(self):
+        pass
+    pass
