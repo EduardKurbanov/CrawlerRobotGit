8895471d1d76d54a4f3f2ccf021704331b68ddc8
Edward <edward89@ukr.net>
2023-12-13 21:18:25 +0200

    fix tegister bit and add def axis_x_y_z

--- compass_magnetometer_qmc5883l.py
+++ compass_magnetometer_qmc5883l.py
@@ -20,25 +20,28 @@ class QMC5883L(object):
     _REG_QMC5883L_DATA_OUTPUT_Z_LSB_L = 0x04
     _REG_QMC5883L_DATA_OUTPUT_Z_MSB_H = 0x05
     _REG_QMC5886L_STATUS = 0x06
-    _REG_QMC5883L_TEMP_OUT_H = 0X07
-    _REG_QMC5883L_TEMP_OUT_L = 0X08
+    _REG_QMC5883L_TEMP_OUT_H = 0x08
+    _REG_QMC5883L_TEMP_OUT_L = 0x07
     _REG_QMC5883L_CONF_1 = 0x09
     _REG_QMC5883L_CONF_2 = 0x0A
     _REG_QMC5883L_SET_RESET_PERIOD = 0x0B
     _REG_QMC5883L_RESERVED = 0x0C
     _CHIP_ID = 0x0D
     _REG_DATA_UPDATE_RATE_ODR_10HZ = 0x00
-    _REG_DATA_UPDATE_RATE_ODR_50HZ = 0x08
-    _REG_DATA_UPDATE_RATE_ODR_100HZ = 0x10
-    _REG_DATA_UPDATE_RATE_ODR_200HZ = 0x18
-    _REG_OVERSAMPLING_OSR_64 = 0x180
-    _REG_OVERSAMPLING_OSR_128 = 0x100
-    _REG_OVERSAMPLING_OSR_256 = 0x80
+    _REG_DATA_UPDATE_RATE_ODR_50HZ = 0x04
+    _REG_DATA_UPDATE_RATE_ODR_100HZ = 0x08
+    _REG_DATA_UPDATE_RATE_ODR_200HZ = 0x0C
+    _REG_OVERSAMPLING_OSR_64 = 0xC0
+    _REG_OVERSAMPLING_OSR_128 = 0x80
+    _REG_OVERSAMPLING_OSR_256 = 0x40
     _REG_OVERSAMPLING_OSR_512 = 0x00
     _REG_FULL_SCALE_RNG_2G = 0x00
-    _REG_FULL_SCALE_RNG_8G = 0x20
+    _REG_FULL_SCALE_RNG_8G = 0x10
     _REG_MODE_CONTROL_STANDBY = 0x00
     _REG_MODE_CONTROL_CONTINUOUS = 0x01
+    _FIELD_RANGE_SENSITIVITY_2G_12000LSB_G = 12000
+    _FIELD_RANGE_SENSITIVITY_8G_3000LSB_G = 3000
+    _TEMPERATURE_SENSOR_SENSITIVITY = 100
 
 
     def __init__(self, i2c: int = 2):
@@ -52,11 +55,12 @@ class QMC5883L(object):
                                      self._REG_FULL_SCALE_RNG_8G)
         self.i2c_bus.write_byte_data(self.device_address, self._REG_QMC5883L_CONF_1,
                                      self._REG_OVERSAMPLING_OSR_256)
+        self.sensitivity_mag = self._FIELD_RANGE_SENSITIVITY_8G_3000LSB_G
         time.sleep(1)
 
-    def __convert_data_register_shift(self, address_devise=0, high_bit=0, low_bit=0):
-        high_bit_ = self.i2c_bus.read_byte_data(address_devise, high_bit)
-        low_bit_ = self.i2c_bus.read_byte_data(address_devise, low_bit)
+    def __convert_data_register_shift(self, high_bit=0, low_bit=0):
+        high_bit_ = self.i2c_bus.read_byte_data(self.address_devise, high_bit)
+        low_bit_ = self.i2c_bus.read_byte_data(self.address_devise, low_bit)
         signed_int_ = c_short((high_bit_ << 8) | low_bit_).value
         return signed_int_
 
@@ -64,10 +68,21 @@ class QMC5883L(object):
         """
         :return: temp
         """
-        temp_data_signed_int_ = self.__convert_data_register_shift(self.device_address,
-                                                                   self._REG_QMC5883L_TEMP_OUT_H,
+        temp_data_signed_int_ = self.__convert_data_register_shift(self._REG_QMC5883L_TEMP_OUT_H,
                                                                    self._REG_QMC5883L_TEMP_OUT_L)
-        temp = ((temp_data_signed_int_) / 100) + 36.53
+        temp = ((temp_data_signed_int_) / self._TEMPERATURE_SENSOR_SENSITIVITY)
         return temp
+
+    def get_axis_x_y_z() -> Tuple[float, float, float]:
+        """
+        :return: (x, y, z)
+        """
+        x = __convert_data_register_shift(self._REG_QMC5883L_DATA_OUTPUT_X_MSB_H, 
+            self._REG_QMC5883L_DATA_OUTPUT_X_LSB_L) / self.sensitivity_mag
+        y = __convert_data_register_shift(self._REG_QMC5883L_DATA_OUTPUT_Y_MSB_H, 
+            self._REG_QMC5883L_DATA_OUTPUT_Y_LSB_L) / self.sensitivity_mag
+        z = __convert_data_register_shift(self._REG_QMC5883L_DATA_OUTPUT_Z_MSB_H, 
+            self._REG_QMC5883L_DATA_OUTPUT_Z_LSB_L) / self.sensitivity_mag
+        return (x, y ,z)
 
 
