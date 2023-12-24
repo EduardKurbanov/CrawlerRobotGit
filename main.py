252e044c7f98bc429196b393dd29676e54937b5f
Edward <edward89@ukr.net>
2023-12-24 14:02:03 +0200

    add in main import

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# import robot
+import accelerometer_gyroscope_mpu6050
+import compass_magnetometer_qmc5883l
+import ads1115
+import driver_motor_dvr8833
+import servo_sg_90
+import sonar_hy_srf05
 
 def main():
     pass
