29b5a0b4c614af86b084b7c912d3bab8053c1e79
Edward <edward89@ukr.net>
2024-01-02 23:08:02 +0200

    add class ino219

---
+++ ino219.py
@@ -0,0 +1,14 @@
+from typing import Tuple, List
+from ctypes import c_short
+import smbus2
+import time
+import math
+
+
+class INO219(object):
+    def __init__(self):
+        pass
+
+    def get_current_measurement(self):
+        pass
+
