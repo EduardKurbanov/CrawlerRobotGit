226d7beae01fd65a5e53ca2ca42eb99602465373
Edward <edward89@ukr.net>
2024-01-02 14:52:50 +0200

    fix servo

--- servo_sg_90.py
+++ servo_sg_90.py
@@ -1,37 +1,49 @@
-from typing import Tuple
+from typing import Tuple, List
 from ctypes import c_short
 import Adafruit_BBIO.PWM as PWM
 import time
 
 
 class SG90(object):
-    head_servo_x_pwm = 'P2_1'
-    head_servo_y_pwm = 'P2_3'
     __DUTY_MIN = 4.2
     __DUTY_MAX = 12.2
     __DUTY_CONST = 8.2
-    __ARR_DATA = []
+    __ARR_DATA_ANGEL_SONAR = []
 
     def __init__(self):
         print('---init ports sg90---')
+        self.head_servo_x_pwm = 'P2_1'
+        self.head_servo_y_pwm = 'P2_3'
         PWM.start(self.head_servo_x_pwm, self.__DUTY_CONST, 50)
         PWM.start(self.head_servo_y_pwm, self.__DUTY_CONST, 50)
 
-    def _set_servo_position(self, pwm_pin, axis_value, sonar):
+    def __set_servo_position(self, pwm_pin, axis_value, sonar) -> Tuple[int, float] or str:
         if not (-90.0 <= axis_value <= 90.0):
             return f"error angle {axis_value}"
         duty_cycle = self.__DUTY_MIN + (self.__DUTY_MAX - self.__DUTY_MIN) * ((axis_value + 90) / 180.0)
         print(duty_cycle)
         PWM.set_duty_cycle(pwm_pin, duty_cycle)
-        tuple_data = (axis_value, sonar)
-        self.__ARR_DATA.append(tuple_data)
-        return tuple_data
+        tuple_data_angle_sonar = (axis_value, sonar)
+        self.__ARR_DATA_ANGEL_SONAR.append(tuple_data_angle_sonar)
+        return tuple_data_angle_sonar
 
-    def servo_axis_x(self, axis_x: float = 0.0, sonar: float = 0.0):
-        return self._set_servo_position(self.head_servo_x_pwm, axis_x, sonar)
+    def get_data_list_angle_sonar(self) -> List:
+        """
+        return list -> [(angle, sonar), ...]
+        """
+        return self.__ARR_DATA_ANGEL_SONAR
 
-    def servo_axis_y(self, axis_y: float = 0.0, sonar: float = 0.0):
-        return self._set_servo_position(self.head_servo_y_pwm, axis_y, sonar)
+    def servo_axis_x(self, axis_x: float = 0.0, sonar: float = 0.0) -> Tuple[int, float] or str:
+        """
+        return tuple -> (angle, sonar)
+        """
+        return self.__set_servo_position(self.head_servo_x_pwm, axis_x, sonar)
+
+    def servo_axis_y(self, axis_y: float = 0.0, sonar: float = 0.0) -> Tuple[int, float] or str:
+        """
+        return tuple -> (angle, sonar)
+        """
+        return self.__set_servo_position(self.head_servo_y_pwm, axis_y, sonar)
 
 
 # Example usage:
